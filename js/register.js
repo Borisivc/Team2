a4358333b32a3f8971586e5048564e6825918885
Libena <li.jara@duocuc.cl>
2022-05-22 22:10:38 -0400

    validaciones

--- js/register.js
+++ js/register.js
@@ -1,39 +1,56 @@
-$("#registro").validate({
-    rules:{
-      username: "required",
-      password: "required"
-    },
-    messages: {
-      username: "Ingresar el nombre de usuario",
-      password: "Ingresar la contraseña"
-    }
-  });
+jQuery.validator.addMethod("noSpace", function(value, element) { 
+    return value == '' || value.trim().length != 0;  
+}, "No space please and don't leave it empty");
+jQuery.validator.addMethod("customEmail", function(value, element) { 
+  return this.optional( element ) || /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/.test( value ); 
+}, "Please enter valid email address!");
+$.validator.addMethod( "alphanumeric", function( value, element ) {
+return this.optional( element ) || /^\w+$/i.test( value );
+}, "Letters, numbers, and underscores only please" );
+$(function(){
+    var $ingreso = $("#ingreso");
+    if($ingreso.length){
+        $ingreso.validate({
+            rules:{
+                username: {
+                    required: true,
+                    alphanumeric: true
+                },
+                password: {
+                    required: true
+                },  
+            },
+            messages:{
+                username: {
+                    required: 'Por favor ingrese usuario'
+                },
+                password: {
+                    required: 'Por favor ingrese contraseña'
+                }
 
-$("#formRegistro").validate({
-  rules:{
-    username: {
-      required: true,
-      minlength: 5,
-      maxlength: 10
-    },
-    email: {
-      required: true,
-      email: true
-    },
-    password1: {
-      required: true,
-      minlength: 10
-    },
-    password2:  {
-      required: true,
-      minlength: 10,
-      equalTo: "#password1"
-    },
-  },
-  messages: {
-    username: {
-      minlength: "Ingrese un Minimo de 5 caracteres",
-      maxlength: "Debe contener maximo 10 caracteres"
-    },
-  }
-});
+            }
+        }) 
+    }
+})
+var $registrationForm = $('#registration');
+if($registrationForm.length){
+  $registrationForm.validate({
+      rules:{
+          username2: {
+              required: true,
+              alphanumeric: true
+          },
+          email: {
+              required: true,
+              customEmail: true
+          },
+          password2: {
+              required: true
+          },
+          confirm: {
+              required: true,
+              equalTo: '#password'
+          },
+        }
+  });
+}
