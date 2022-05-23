7a079b1ba03237b2f78cf3e42a80158703ebddc1
Libena <li.jara@duocuc.cl>
2022-05-22 20:56:35 -0400

    cambios

---
+++ js/register.js
@@ -0,0 +1,39 @@
+$("#registro").validate({
+    rules:{
+      username: "required",
+      password: "required"
+    },
+    messages: {
+      username: "Ingresar el nombre de usuario",
+      password: "Ingresar la contraseña"
+    }
+  });
+
+$("#formRegistro").validate({
+  rules:{
+    username: {
+      required: true,
+      minlength: 5,
+      maxlength: 10
+    },
+    email: {
+      required: true,
+      email: true
+    },
+    password1: {
+      required: true,
+      minlength: 10
+    },
+    password2:  {
+      required: true,
+      minlength: 10,
+      equalTo: "#password1"
+    },
+  },
+  messages: {
+    username: {
+      minlength: "Ingrese un Minimo de 5 caracteres",
+      maxlength: "Debe contener maximo 10 caracteres"
+    },
+  }
+});
