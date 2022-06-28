fc648bece13110c3922645bd66236f46c3ca54f5
Libena <li.jara@duocuc.cl>
2022-06-28 10:07:46 -0400

    usuario

--- Spotifly/settings.py
+++ Spotifly/settings.py
@@ -38,6 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'blog.apps.BlogConfig',
+    'rest_framework',
+    'rest_framework.authtoken',
+    'profiles_api',
+    
 ]
 
 MIDDLEWARE = [
---
+++ profiles_api/models.py
@@ -0,0 +1,6 @@
+from django.db import models
+from django.contrib.auth.models import AbstractBaseUser
+
+# Creando los modelos para los usuarios, iniciando con email
+
+
