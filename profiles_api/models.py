13a8a751566dcc10f1d28d569f27c7db90758218
Libena <li.jara@duocuc.cl>
2022-06-28 10:19:08 -0400

    uu

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -1,6 +1,29 @@
+from unicodedata import name
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.models import PermissionsMixin
 
 # Creando los modelos para los usuarios, iniciando con email
 
-
+class UserProfile(AbstractBaseUser,PermissionsMixin):
+    """Modelo base de datos para usuarios"""
+    email = models.EmailField(max_length=255, unique=True)
+    name= models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    
+    objects = UserProfileManager()
+    
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['name']
+    
+    def get_full_name(self):
+        """Obtener nombre completo"""
+        return self.name
+    
+    def get_short_name(self):
+        """Obtener nombre corto"""
+        return self.name
+    
+    def __str__(self):
+        return self.email
