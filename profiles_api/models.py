fd822db602412b1e503c452aa61fadda3410754f
Libena <li.jara@duocuc.cl>
2022-06-28 10:26:34 -0400

    creando user

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -2,9 +2,32 @@ from unicodedata import name
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser
 from django.contrib.auth.models import PermissionsMixin
+from django.contrib.auth.models import BaseUserManager
 
 # Creando los modelos para los usuarios, iniciando con email
-
+class UserProfileManager(BaseUserManager):
+    def create_user(self, email, name, password=None):
+        """Crear nuevo usur profile"""
+        if not email:
+            raise ValueError('Debe registrar un email')
+        email= self.normalize_email(email)
+        user = self.model(email= email, name=name)
+        
+        user.set_password(password)
+        user.save(using=self._db)
+        
+        return user
+    
+    def create_superuser(self, email, name, password):
+        user = self.create_user(email, name, password)
+        
+        user.is_superiser = True
+        user.is_staff = True
+        user.save(using=self._db)
+        
+        return user
+    
+                             
 class UserProfile(AbstractBaseUser,PermissionsMixin):
     """Modelo base de datos para usuarios"""
     email = models.EmailField(max_length=255, unique=True)
